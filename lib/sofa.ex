cfebb913de359bddfaf8b5194eb62387ca6c05d5
Dave Cottlehuber <dch@skunkwerks.at>
2021-04-09 13:05:49 +0000

    :mix new --sup sofa

---
+++ lib/sofa.ex
@@ -0,0 +1,18 @@
+defmodule Sofa do
+  @moduledoc """
+  Documentation for `Sofa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sofa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
