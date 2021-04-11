719c135370bbdf4b483883f641edeca2439f6e60
Dave Cottlehuber <dch@skunkwerks.at>
2021-04-11 16:01:13 +0000

    add sofa.init/1 uri parser

--- test/sofa_test.exs
+++ test/sofa_test.exs
@@ -2,7 +2,23 @@ defmodule SofaTest do
   use ExUnit.Case
   doctest Sofa
 
-  test "greets the world" do
-    assert Sofa.hello() == :world
+  test "init returns expected struct" do
+    assert Sofa.init() == %Sofa{
+             auth: "admin:passwd",
+             features: nil,
+             uri: %URI{
+               authority: "admin:passwd@localhost:5984",
+               fragment: nil,
+               host: "localhost",
+               path: "/",
+               port: 5984,
+               query: nil,
+               scheme: "http",
+               userinfo: "admin:passwd"
+             },
+             uuid: nil,
+             vendor: nil,
+             version: nil
+           }
   end
 end
